5d634113264c9c036186fca92fd1b2176e0b5201
AnuragSrivastava84 <anurag1984.srivas@gmail.com>
2022-11-15 00:11:03 +0800

    Code to parse URL path
    - Changes to parse the URL path
    - Changes to log the URL path in the console

--- index.mjs
+++ index.mjs
@@ -4,10 +4,22 @@
 
 // Dependencies
 import http from "http";
+import url from "url";
 
 // The server should respond to all the request with string
 const server = http.createServer((req, res) => {
+  // Get the URL and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get the path from the URL
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  // Send the response
   res.end("Hello world!\n");
+
+  // Log the request path
+  console.log("Request is received on path: " + trimmedPath);
 });
 
 // The server should listen on port 3000
